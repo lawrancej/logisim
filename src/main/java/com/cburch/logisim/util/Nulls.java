23d210c2f744f50bab00a43f82f17e4ef4f68375
Joey Lawrance <lawrancej@wit.edu>
2012-10-19 11:02:17 -0400

    Tested existing Nulls class, deprecated it.

--- src/main/java/com/cburch/logisim/util/Nulls.java
+++ src/main/java/com/cburch/logisim/util/Nulls.java
@@ -3,26 +3,32 @@
 
 package com.cburch.logisim.util;
 
+/**
+ * Null safe equality testing class.
+ * Obsolete in Java 7, thanks to static method java.util.Objects.equal(a,b).
+ * Until Java 7 becomes popular, use Commons Lang ObjectUtils.equal instead.
+ * @author Joey Lawrance
+ *
+ */
 public final class Nulls {
-	private Nulls() { }
 	
+	/**
+	 * Null safe equality test equivalent to java.util.Objects.equal or ObjectUtils.equal
+	 * @param a
+	 * @param b
+	 * @return true if a 
+	 */
+	@Deprecated
 	public static boolean equal(Object a, Object b) {
-		if(a == null) {
-			return b == null;
-		} else if(b == null) {
-			return false;
+		if(a == null || b == null) {
+			return a == b;
 		} else {
 			return a.equals(b);
 		}
 	}
 	
+	@Deprecated
 	public static boolean unequal(Object a, Object b) {
-		if(a == null) {
-			return b != null;
-		} else if(b == null) {
-			return true;
-		} else {
-			return !a.equals(b);
-		}
+		return !equal(a,b);
 	}
 }
