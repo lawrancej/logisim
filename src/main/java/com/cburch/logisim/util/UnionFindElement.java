e2cec8e278752d30ea5c171986126567507e574c
Joey Lawrance <lawrancej@wit.edu>
2012-10-30 22:38:20 -0400

    Removed UnionFind, because nobody used it.

--- src/main/java/com/cburch/logisim/util/UnionFindElement.java
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 2010, Carl Burch. License information is located in the
- * com.cburch.logisim.Main source code and at www.cburch.com/logisim/. */
-
-package com.cburch.logisim.util;
-
-public interface UnionFindElement<E> {
-	public E getUnionFindParent();
-	public void setUnionFindParent(E value);
-}
