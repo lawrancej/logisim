b253d165d4836a1e7209418c0251a4020b40be4f
allevaton <nicholas.allevato@gmail.com>
2014-01-23 14:12:55 -0500

    Revert "delete"
    This reverts commit 099025e8880590a8ab8fae575feb7ff1c75d8d34.

---
+++ src/main/java/com/cburch/gray/CounterData.java
@@ -0,0 +1,70 @@
+/* Copyright (c) 2010, Carl Burch. License information is located in the
+ * com.cburch.logisim.Main source code and at www.cburch.com/logisim/. */
+
+package com.cburch.gray;
+
+import com.cburch.logisim.data.BitWidth;
+import com.cburch.logisim.data.Value;
+import com.cburch.logisim.instance.InstanceData;
+import com.cburch.logisim.instance.InstanceState;
+
+/** Represents the state of a counter. */
+class CounterData implements InstanceData, Cloneable {
+	/** Retrieves the state associated with this counter in the circuit state,
+	 * generating the state if necessary.
+	 */
+	public static CounterData get(InstanceState state, BitWidth width) {
+		CounterData ret = (CounterData) state.getData();
+		if (ret == null) {
+			// If it doesn't yet exist, then we'll set it up with our default
+			// values and put it into the circuit state so it can be retrieved
+			// in future propagations.
+			ret = new CounterData(null, Value.createKnown(width, 0));
+			state.setData(ret);
+		} else if (!ret.value.getBitWidth().equals(width)) {
+			ret.value = ret.value.extendWidth(width.getWidth(), Value.FALSE);
+		}
+		return ret;
+	}
+
+	/** The last clock input value observed. */
+	private Value lastClock;
+	
+	/** The current value emitted by the counter. */
+	private Value value;
+
+	/** Constructs a state with the given values. */
+	public CounterData(Value lastClock, Value value) {
+		this.lastClock = lastClock;
+		this.value = value;
+	}
+
+	/** Returns a copy of this object. */
+	@Override
+	public Object clone() {
+		// We can just use what super.clone() returns: The only instance variables are
+		// Value objects, which are immutable, so we don't care that both the copy
+		// and the copied refer to the same Value objects. If we had mutable instance
+		// variables, then of course we would need to clone them.
+		try { return super.clone(); }
+		catch (CloneNotSupportedException e) { return null; }
+	}
+	
+	/** Updates the last clock observed, returning true if triggered. */
+	public boolean updateClock(Value value) {
+		Value old = lastClock;
+		lastClock = value;
+		return old == Value.FALSE && value == Value.TRUE;
+	}
+	
+	/** Returns the current value emitted by the counter. */
+	public Value getValue() {
+		return value;
+	}
+	
+	/** Updates the current value emitted by the counter. */
+	public void setValue(Value value) {
+		this.value = value;
+	}
+}
+
---
+++ src/main/java/com/cburch/gray/GrayCounter.java
@@ -0,0 +1,97 @@
+/* Copyright (c) 2010, Carl Burch. License information is located in the
+ * com.cburch.logisim.Main source code and at www.cburch.com/logisim/. */
+
+package com.cburch.gray;
+
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+
+import com.cburch.logisim.data.Attribute;
+import com.cburch.logisim.data.BitWidth;
+import com.cburch.logisim.data.Bounds;
+import com.cburch.logisim.data.Direction;
+import com.cburch.logisim.instance.Instance;
+import com.cburch.logisim.instance.InstanceFactory;
+import com.cburch.logisim.instance.InstancePainter;
+import com.cburch.logisim.instance.InstanceState;
+import com.cburch.logisim.instance.Port;
+import com.cburch.logisim.instance.StdAttr;
+import com.cburch.logisim.util.GraphicsUtil;
+import com.cburch.logisim.util.StringUtil;
+
+/** Manufactures a counter that iterates over Gray codes. This demonstrates
+ * several additional features beyond the SimpleGrayCounter class. */
+class GrayCounter extends InstanceFactory {
+	public GrayCounter() {
+		super("Gray Counter");
+		setOffsetBounds(Bounds.create(-30, -15, 30, 30));
+		setPorts(new Port[] {
+				new Port(-30, 0, Port.INPUT, 1),
+				new Port(  0, 0, Port.OUTPUT, StdAttr.WIDTH),
+		});
+		
+		// We'll have width, label, and label font attributes. The latter two
+		// attributes allow us to associate a label with the component (though
+		// we'll also need configureNewInstance to configure the label's
+		// location).
+		setAttributes(
+				new Attribute[] { StdAttr.WIDTH, StdAttr.LABEL, StdAttr.LABEL_FONT },
+				new Object[] { BitWidth.create(4), "", StdAttr.DEFAULT_LABEL_FONT });
+		
+		// The following method invocation sets things up so that the instance's
+		// state can be manipulated using the Poke Tool.
+		setInstancePoker(CounterPoker.class);
+		
+		// These next two lines set it up so that the explorer window shows a
+		// customized icon representing the component type. This should be a
+		// 16x16 image.
+		URL url = getClass().getClassLoader().getResource("com/cburch/gray/counter.gif");
+		if (url != null) setIcon(new ImageIcon(url));
+	}
+	
+	/** The configureNewInstance method is invoked every time a new instance
+	 * is created. In the superclass, the method doesn't do anything, since
+	 * the new instance is pretty thoroughly configured already by default. But
+	 * sometimes you need to do something particular to each instance, so you
+	 * would override the method. In this case, we need to set up the location
+	 * for its label. */
+	@Override
+	protected void configureNewInstance(Instance instance) {
+		Bounds bds = instance.getBounds();
+		instance.setTextField(StdAttr.LABEL, StdAttr.LABEL_FONT,
+				bds.getX() + bds.getWidth() / 2, bds.getY() - 3,
+				GraphicsUtil.H_CENTER, GraphicsUtil.V_BASELINE);
+	}
+
+	@Override
+	public void propagate(InstanceState state) {
+		// This is the same as with SimpleGrayCounter, except that we use the
+		// StdAttr.WIDTH attribute to determine the bit width to work with.
+		BitWidth width = state.getAttributeValue(StdAttr.WIDTH);
+		CounterData cur = CounterData.get(state, width);
+		boolean trigger = cur.updateClock(state.getPort(0));
+		if (trigger) cur.setValue(GrayIncrementer.nextGray(cur.getValue()));
+		state.setPort(1, cur.getValue(), 9);
+	}
+
+	@Override
+	public void paintInstance(InstancePainter painter) {
+		// This is essentially the same as with SimpleGrayCounter, except for
+		// the invocation of painter.drawLabel to make the label be drawn.
+		painter.drawBounds();
+		painter.drawClock(0, Direction.EAST);
+		painter.drawPort(1);
+		painter.drawLabel();
+		
+		if (painter.getShowState()) {
+			BitWidth width = painter.getAttributeValue(StdAttr.WIDTH);
+			CounterData state = CounterData.get(painter, width);
+			Bounds bds = painter.getBounds();
+			GraphicsUtil.drawCenteredText(painter.getGraphics(),
+					StringUtil.toHexString(width.getWidth(), state.getValue().toIntValue()),
+					bds.getX() + bds.getWidth() / 2,
+					bds.getY() + bds.getHeight() / 2);
+		}
+	}
+}
