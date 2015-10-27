996a10189a85c0771b49b90be5b97e633ea6e633
Francisco Madrid <fjmadrid@gmail.com>
2015-10-27 12:33:49 -0300

    adding breadboard component visualization

--- src/main/java/cl/uchile/dcc/cc4401/protosim/libraries/Protosim.java
+++ src/main/java/cl/uchile/dcc/cc4401/protosim/libraries/Protosim.java
@@ -1,37 +1,37 @@
 package cl.uchile.dcc.cc4401.protosim.libraries;
 
+import java.util.Arrays;
 import java.util.List;
 
-import com.cburch.logisim.std.wiring.Wiring;
-import com.cburch.logisim.tools.FactoryDescription;
+import com.cburch.logisim.tools.AddTool;
 import com.cburch.logisim.tools.Library;
 import com.cburch.logisim.tools.Tool;
 
+import cl.uchile.dcc.cc4401.protosim.components.Breadboard;
+
 public class Protosim extends Library {
-	
-    private static FactoryDescription[] DESCRIPTIONS = {
-    		// TODO: l10n this
-            // new FactoryDescription("Power", getFromLocale("powerComponent"), "power.svg", "Power"),
-            new FactoryDescription("Power", "Voltage generator", "power.svg", "Power"),
-    };    
-    
+
+    private static Tool[] TOOLS = {
+            new AddTool(Breadboard.FACTORY),
+    };
+
     public Protosim() {
     }
 
     @Override
     public String getName() {
-    	return "Protosim";
+        return "Protosim";
     }
 
     @Override
     public String getDisplayName() {
-    	// TODO: l10n this
-    	// return getFromLocale("protosimLibrary");
-    	return "Protosim";
+        // TODO: l10n this
+        // return getFromLocale("protosimLibrary");
+        return "Protosim";
     }
 
     @Override
     public List<Tool> getTools() {
-    	return FactoryDescription.getTools(Wiring.class, DESCRIPTIONS);
+        return Arrays.asList(TOOLS);
     }
 }
