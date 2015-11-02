cce1a033b6fb3433e62b509626684ebbf2f9d94c
theUnknown <mariocesarcornejo@gmail.com>
2015-11-02 01:38:28 -0300

    issue #23 ahora todos los pines correspondientes estan conectados internamente

--- src/main/java/cl/uchile/dcc/cc4401/protosim/components/TunnelProtoboard.java
+++ src/main/java/cl/uchile/dcc/cc4401/protosim/components/TunnelProtoboard.java
@@ -14,86 +14,122 @@ import com.cburch.logisim.instance.InstanceState;
 import com.cburch.logisim.instance.Port;
 
 public class TunnelProtoboard extends InstanceFactory {
-    
-    public static InstanceFactory FACTORY = new TunnelProtoboard();
-    List<Port> ports;
-    HashMap<Port,Integer> connected;//si dos ports tienen el mismo valor estan conectados
-    
-    public TunnelProtoboard() {
-        super("TunnelProtoboard");
-        
-        ports = new ArrayList<Port>();
-        connected=new HashMap<Port,Integer>();
-        for (int i = 0; i <= 64 * 10; i += 10) {
-            for (int j = 0; j < 10; j += 10) {
-            	Port port1=new Port(10 + i, 10 + j, Port.INOUT, 1);
-            	Port port2=new Port(10 + i, 20 + j, Port.INOUT, 1);
-                connected.put(port1,new Integer(1));
-                connected.put(port2,new Integer(2));
-                ports.add(port1);
-                ports.add(port2);
-                
-                Port port3=new Port(10 + i, 170 + j, Port.INOUT, 1);
-            	Port port4=new Port(10 + i, 180 + j, Port.INOUT, 1);
-                connected.put(port3,new Integer(3));
-                connected.put(port4,new Integer(4));
-                ports.add(port3);
-                ports.add(port4);
-            }
-        }
-        
-        //falta agregar estos ports al Hash con el mismo valor int si es que estan conectados
-        //
-        for (int i = 0; i <= 64 * 10; i += 10) {
-            for (int j = 0; j < 4 * 10; j += 10) {
-                ports.add(new Port(10 + i, 50 + j, Port.INOUT, 1));
-                ports.add(new Port(10 + i, 110 + j, Port.INOUT, 1));
-            }
-        }
-        setPorts(ports);
-        createAttributeSet();
-    }
-    
-    @Override
-    public Bounds getOffsetBounds(AttributeSet attrs) {
-        return Bounds.create(0, 0, 660, 190);
-    }
 
-    @Override
-    public void paintInstance(InstancePainter painter) {
-        Location loc = painter.getLocation();
-        int x = loc.getX();
-        int y = loc.getY();
+	public static InstanceFactory FACTORY = new TunnelProtoboard();
+	List<Port> ports;
+	HashMap<Port, Integer> connected;// si dos ports tienen el mismo valor estan
+										// conectados
 
-        Graphics g = painter.getGraphics();
-        g.setColor(Color.black);
-        g.drawRect(x, y, 660, 190);
-        
-        //las lineas tipicas de las proto
-        g.setColor(Color.red);
-        g.drawLine(x+10,y+26, x+650, y+26);
-        g.drawLine(x+10,y+186, x+650, y+186);
-        g.setColor(Color.blue);
-        g.drawLine(x+10,y+3, x+650, y+3);
-        g.drawLine(x+10,y+164, x+650, y+164);
-        
-        painter.drawPorts();
-    }
+	public TunnelProtoboard() {
+		super("TunnelProtoboard");
 
-    @Override
-    public void propagate(InstanceState state) {
-        // TODO Auto-generated method stub  
-    }
-    
-    public HashMap<Port,Integer> getConnected(){
-    	return connected;
-    }
-    
-    public AttributeSet createAttributeSet() {
-        TunnelProtoboardAttributes atts= new TunnelProtoboardAttributes();
-        atts.setConnected(connected);
-        atts.setPorts(ports);
-        return atts;
-    }
+		ports = new ArrayList<Port>();
+		connected = new HashMap<Port, Integer>();
+
+		int pinGroup = 1;
+
+		for (int i = 0; i <= 64 * 10; i += 10) {
+			Port port = new Port(10 + i, 10, Port.INOUT, 1);
+			ports.add(port);
+			connected.put(port, pinGroup);
+
+		}
+		pinGroup++;
+		for (int i = 0; i <= 64 * 10; i += 10) {
+			Port port = new Port(10 + i, 20, Port.INOUT, 1);
+			ports.add(port);
+			connected.put(port, pinGroup);
+		}
+		pinGroup++;
+		for (int i = 0; i <= 64 * 10; i += 10) {
+			Port port = new Port(10 + i, 170, Port.INOUT, 1);
+			ports.add(port);
+			connected.put(port, pinGroup);
+		}
+		pinGroup++;
+		for (int i = 0; i <= 64 * 10; i += 10) {
+			Port port = new Port(10 + i, 180, Port.INOUT, 1);
+			ports.add(port);
+			connected.put(port, pinGroup);
+		}
+		pinGroup++;
+
+		for (int i = 0; i <= 64 * 10; i += 10) {
+			for (int j = 0; j < 4 * 10; j += 10) {
+				Port port1 = new Port(10 + i, 50, Port.INOUT, 1);
+				Port port2 = new Port(10 + i, 50 + 10, Port.INOUT, 1);
+				Port port3 = new Port(10 + i, 50 + 20, Port.INOUT, 1);
+				Port port4 = new Port(10 + i, 50 + 30, Port.INOUT, 1);
+				Port port5 = new Port(10 + i, 110, Port.INOUT, 1);
+				Port port6 = new Port(10 + i, 110 + 10, Port.INOUT, 1);
+				Port port7 = new Port(10 + i, 110 + 20, Port.INOUT, 1);
+				Port port8 = new Port(10 + i, 110 + 30, Port.INOUT, 1);
+
+				ports.add(port1);
+				ports.add(port2);
+				ports.add(port3);
+				ports.add(port4);
+				ports.add(port5);
+				ports.add(port6);
+				ports.add(port7);
+				ports.add(port8);
+
+				connected.put(port1, pinGroup);
+				connected.put(port2, pinGroup);
+				connected.put(port3, pinGroup);
+				connected.put(port4, pinGroup);
+				pinGroup++;
+				connected.put(port5, pinGroup);
+				connected.put(port6, pinGroup);
+				connected.put(port7, pinGroup);
+				connected.put(port8, pinGroup);
+				pinGroup++;
+			}
+		}
+		setPorts(ports);
+		createAttributeSet();
+	}
+
+	@Override
+	public Bounds getOffsetBounds(AttributeSet attrs) {
+		return Bounds.create(0, 0, 660, 190);
+	}
+
+	@Override
+	public void paintInstance(InstancePainter painter) {
+		Location loc = painter.getLocation();
+		int x = loc.getX();
+		int y = loc.getY();
+
+		Graphics g = painter.getGraphics();
+		g.setColor(Color.black);
+		g.drawRect(x, y, 660, 190);
+
+		// las lineas tipicas de las proto
+		g.setColor(Color.red);
+		g.drawLine(x + 10, y + 26, x + 650, y + 26);
+		g.drawLine(x + 10, y + 186, x + 650, y + 186);
+		g.setColor(Color.blue);
+		g.drawLine(x + 10, y + 3, x + 650, y + 3);
+		g.drawLine(x + 10, y + 164, x + 650, y + 164);
+
+		painter.drawPorts();
+	}
+
+	@Override
+	public void propagate(InstanceState state) {
+		// TODO Auto-generated method stub
+	}
+
+	public HashMap<Port, Integer> getConnected() {
+		return connected;
+	}
+
+	public AttributeSet createAttributeSet() {
+		TunnelProtoboardAttributes atts = new TunnelProtoboardAttributes();
+		atts.setConnected(connected);
+		atts.setPorts(ports);
+		return atts;
+	}
 
 }
