9997df0cf36b506e876632478f36595e9e121c4e
theUnknown <mariocesarcornejo@gmail.com>
2015-11-02 03:28:06 -0300

    se arreglo un bug, se estaban creando mas Ports de los necesarios uno encima del otro

--- src/main/java/cl/uchile/dcc/cc4401/protosim/components/TunnelProtoboard.java
+++ src/main/java/cl/uchile/dcc/cc4401/protosim/components/TunnelProtoboard.java
@@ -32,7 +32,6 @@ public class TunnelProtoboard extends InstanceFactory {
 			Port port = new Port(10 + i, 10, Port.INOUT, 1);
 			ports.add(port);
 			connected.put(port, pinGroup);
-
 		}
 		pinGroup++;
 		for (int i = 0; i <= 64 * 10; i += 10) {
@@ -55,36 +54,32 @@ public class TunnelProtoboard extends InstanceFactory {
 		pinGroup++;
 
 		for (int i = 0; i <= 64 * 10; i += 10) {
-			for (int j = 0; j < 4 * 10; j += 10) {
-				Port port1 = new Port(10 + i, 50, Port.INOUT, 1);
-				Port port2 = new Port(10 + i, 50 + 10, Port.INOUT, 1);
-				Port port3 = new Port(10 + i, 50 + 20, Port.INOUT, 1);
-				Port port4 = new Port(10 + i, 50 + 30, Port.INOUT, 1);
-				Port port5 = new Port(10 + i, 110, Port.INOUT, 1);
-				Port port6 = new Port(10 + i, 110 + 10, Port.INOUT, 1);
-				Port port7 = new Port(10 + i, 110 + 20, Port.INOUT, 1);
-				Port port8 = new Port(10 + i, 110 + 30, Port.INOUT, 1);
-
-				ports.add(port1);
-				ports.add(port2);
-				ports.add(port3);
-				ports.add(port4);
-				ports.add(port5);
-				ports.add(port6);
-				ports.add(port7);
-				ports.add(port8);
-
-				connected.put(port1, pinGroup);
-				connected.put(port2, pinGroup);
-				connected.put(port3, pinGroup);
-				connected.put(port4, pinGroup);
-				pinGroup++;
-				connected.put(port5, pinGroup);
-				connected.put(port6, pinGroup);
-				connected.put(port7, pinGroup);
-				connected.put(port8, pinGroup);
-				pinGroup++;
-			}
+			Port port1 = new Port(10 + i, 50, Port.INOUT, 1);
+			Port port2 = new Port(10 + i, 50 + 10, Port.INOUT, 1);
+			Port port3 = new Port(10 + i, 50 + 20, Port.INOUT, 1);
+			Port port4 = new Port(10 + i, 50 + 30, Port.INOUT, 1);
+			Port port5 = new Port(10 + i, 110, Port.INOUT, 1);
+			Port port6 = new Port(10 + i, 110 + 10, Port.INOUT, 1);
+			Port port7 = new Port(10 + i, 110 + 20, Port.INOUT, 1);
+			Port port8 = new Port(10 + i, 110 + 30, Port.INOUT, 1);
+			ports.add(port1);
+			ports.add(port2);
+			ports.add(port3);
+			ports.add(port4);
+			ports.add(port5);
+			ports.add(port6);
+			ports.add(port7);
+			ports.add(port8);
+			connected.put(port1, pinGroup);
+			connected.put(port2, pinGroup);
+			connected.put(port3, pinGroup);
+			connected.put(port4, pinGroup);
+			pinGroup++;
+			connected.put(port5, pinGroup);
+			connected.put(port6, pinGroup);
+			connected.put(port7, pinGroup);
+			connected.put(port8, pinGroup);
+			pinGroup++;
 		}
 		setPorts(ports);
 		createAttributeSet();
