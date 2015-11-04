97d21cc0ea3c2eb91f14ed348e7566ce0f746ef3
Francisco Madrid <fjmadrid@gmail.com>
2015-11-04 01:08:43 -0300

    se utiliza constante para definir ancho de bits de los puertos

--- src/main/java/cl/uchile/dcc/cc4401/protosim/components/Breadboard.java
+++ src/main/java/cl/uchile/dcc/cc4401/protosim/components/Breadboard.java
@@ -16,6 +16,8 @@ import com.cburch.logisim.util.Icons;
 
 public class Breadboard extends InstanceFactory {
 
+	public static final int PORT_WIDTH = 32;
+
 	public static InstanceFactory FACTORY = new Breadboard();
 	
 	private List<Port> ports;
@@ -42,38 +44,38 @@ public class Breadboard extends InstanceFactory {
 	private void createAndConnectPorts() {
 		int pinGroup = 1;
 		for (int i = 0; i <= 64 * 10; i += 10) {
-			Port port = new Port(10 + i, 10, Port.INOUT, 1);
+			Port port = new Port(10 + i, 10, Port.INOUT, PORT_WIDTH);
 			ports.add(port);
 			connected.put(port, pinGroup);
 		}
 		pinGroup++;
 		for (int i = 0; i <= 64 * 10; i += 10) {
-			Port port = new Port(10 + i, 20, Port.INOUT, 1);
+			Port port = new Port(10 + i, 20, Port.INOUT, PORT_WIDTH);
 			ports.add(port);
 			connected.put(port, pinGroup);
 		}
 		pinGroup++;
 		for (int i = 0; i <= 64 * 10; i += 10) {
-			Port port = new Port(10 + i, 170, Port.INOUT, 1);
+			Port port = new Port(10 + i, 170, Port.INOUT, PORT_WIDTH);
 			ports.add(port);
 			connected.put(port, pinGroup);
 		}
 		pinGroup++;
 		for (int i = 0; i <= 64 * 10; i += 10) {
-			Port port = new Port(10 + i, 180, Port.INOUT, 1);
+			Port port = new Port(10 + i, 180, Port.INOUT, PORT_WIDTH);
 			ports.add(port);
 			connected.put(port, pinGroup);
 		}
 		pinGroup++;
 		for (int i = 0; i <= 64 * 10; i += 10) {
-			Port port1 = new Port(10 + i, 50, Port.INOUT, 1);
-			Port port2 = new Port(10 + i, 50 + 10, Port.INOUT, 1);
-			Port port3 = new Port(10 + i, 50 + 20, Port.INOUT, 1);
-			Port port4 = new Port(10 + i, 50 + 30, Port.INOUT, 1);
-			Port port5 = new Port(10 + i, 110, Port.INOUT, 1);
-			Port port6 = new Port(10 + i, 110 + 10, Port.INOUT, 1);
-			Port port7 = new Port(10 + i, 110 + 20, Port.INOUT, 1);
-			Port port8 = new Port(10 + i, 110 + 30, Port.INOUT, 1);
+			Port port1 = new Port(10 + i, 50, Port.INOUT, PORT_WIDTH);
+			Port port2 = new Port(10 + i, 50 + 10, Port.INOUT, PORT_WIDTH);
+			Port port3 = new Port(10 + i, 50 + 20, Port.INOUT, PORT_WIDTH);
+			Port port4 = new Port(10 + i, 50 + 30, Port.INOUT, PORT_WIDTH);
+			Port port5 = new Port(10 + i, 110, Port.INOUT, PORT_WIDTH);
+			Port port6 = new Port(10 + i, 110 + 10, Port.INOUT, PORT_WIDTH);
+			Port port7 = new Port(10 + i, 110 + 20, Port.INOUT, PORT_WIDTH);
+			Port port8 = new Port(10 + i, 110 + 30, Port.INOUT, PORT_WIDTH);
 			ports.add(port1);
 			ports.add(port2);
 			ports.add(port3);
