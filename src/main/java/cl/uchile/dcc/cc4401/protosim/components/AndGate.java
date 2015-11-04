bf8d90e5f420f611430150d947a56b1e6e77faef
Francisco Madrid <fjmadrid@gmail.com>
2015-11-03 23:58:51 -0300

    limpieza del codigo

--- src/main/java/cl/uchile/dcc/cc4401/protosim/components/AndGate.java
+++ src/main/java/cl/uchile/dcc/cc4401/protosim/components/AndGate.java
@@ -3,8 +3,6 @@ package cl.uchile.dcc.cc4401.protosim.components;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.cburch.logisim.data.AttributeSet;
 import com.cburch.logisim.data.Bounds;
@@ -12,25 +10,12 @@ import com.cburch.logisim.data.Location;
 import com.cburch.logisim.instance.InstanceFactory;
 import com.cburch.logisim.instance.InstancePainter;
 import com.cburch.logisim.instance.InstanceState;
-import com.cburch.logisim.instance.Port;
 
 public class AndGate extends InstanceFactory {
 	
-	private List<Port> ports;
-	
 	public AndGate() {
-		super("AndGate");
-		setIconName("p_and.svg");
-		
-		/*ports = new ArrayList<Port>();
-		ports.add(new Port(0, 0, Port.INPUT, 1));
-		ports.add(new Port(10, 0, Port.INPUT, 1));
-		ports.add(new Port(20, 0, Port.OUTPUT, 1));
-		ports.add(new Port(0, 30, Port.INPUT, 1));
-		ports.add(new Port(10, 30, Port.INPUT, 1));
-		ports.add(new Port(20, 30, Port.OUTPUT, 1));
-		setPorts(ports);
-		*/
+		super("ProtosimAndGate");
+		setIconName("protosimComponentGateAnd.svg");
 	}
 
 	@Override
@@ -40,18 +25,25 @@ public class AndGate extends InstanceFactory {
         int y = loc.getY();
 
         Graphics g = painter.getGraphics();
+
+        // Chip
         g.setColor(Color.black);
-        g.fillRect(x, y+5, 20, 20);
-        g.setColor(Color.WHITE);
-        g.setFont(new Font("TimesRoman", Font.PLAIN, 10));
-        g.drawString(" and", x, y+15);
-        g.setColor(Color.GRAY);
-        g.fillRect(x, y, 4, 5);
-        g.fillRect(x+8, y, 4, 5);
-        g.fillRect(x+16, y, 4, 5);
-        g.fillRect(x, y+25, 4, 5);
-        g.fillRect(x+8, y+25, 4, 5);
-        g.fillRect(x+16, y+25, 4, 5);
+        g.fillRect(x - 2, y + 5, 24, 20);
+
+        // Text
+        g.setColor(Color.white);
+        g.setFont(new Font("Courier", Font.BOLD, 9));
+        g.drawString("AND", x + 2, y + 17);
+
+        // Pins
+        g.setColor(Color.gray);
+        g.fillRect(x - 2, y, 4, 5);
+        g.fillRect(x + 8, y, 4, 5);
+        g.fillRect(x + 18, y, 4, 5);
+
+        g.fillRect(x - 2, y + 25, 4, 5);
+        g.fillRect(x + 8, y + 25, 4, 5);
+        g.fillRect(x + 18, y + 25, 4, 5);
         
         painter.drawPorts();
 	}
