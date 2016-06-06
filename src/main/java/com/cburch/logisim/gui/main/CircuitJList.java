cf9cd0eb5f24f649d7b922fceebfa27ed7875f59
Daniel Heid <dheid@posteo.de>
2016-06-06 08:33:06 +0200

    using new  collection type

--- src/main/java/com/cburch/logisim/gui/main/CircuitJList.java
+++ src/main/java/com/cburch/logisim/gui/main/CircuitJList.java
@@ -3,21 +3,21 @@
 
 package com.cburch.logisim.gui.main;
 
-import java.util.Vector;
-
-import javax.swing.JList;
-
 import com.cburch.logisim.circuit.Circuit;
 import com.cburch.logisim.data.Bounds;
 import com.cburch.logisim.file.LogisimFile;
 import com.cburch.logisim.proj.Project;
+
+import javax.swing.JList;
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("serial")
 class CircuitJList extends JList<Circuit> {
     public CircuitJList(Project proj, boolean includeEmpty) {
         LogisimFile file = proj.getLogisimFile();
         Circuit current = proj.getCurrentCircuit();
-        Vector<Circuit> options = new Vector<Circuit>();
+        List<Circuit> options = new ArrayList<>();
         boolean currentFound = false;
         for (Circuit circ : file.getCircuits()) {
             if (!includeEmpty || circ.getBounds() != Bounds.EMPTY_BOUNDS) {
@@ -25,8 +25,7 @@ class CircuitJList extends JList<Circuit> {
                 options.add(circ);
             }
         }
-
-        setListData(options);
+        setListData(options.toArray(new Circuit[options.size()]));
         if (currentFound) setSelectedValue(current, true);
         setVisibleRowCount(Math.min(6, options.size()));
     }
